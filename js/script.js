0682ef42b02f5bffe4991445d305a9aaaeba4869
Francis Pham <fphamsd@gmail.com>
2021-11-30 14:08:14 -0800

    complete index and style. Need function for hamburger menu

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+const hamburger = document.querySelector(".fas");
+const btnPrimary = document.getElementById("btn-primary");
+
+// navToggle.addEventListener("click", function () {
+//   if (links.classList.contains("show-links")) {
+//     btnPrimary.style.backgroundColor = "white";
+//     links.classList.remove("show-links");
+//     hamburger.classList.remove("fa-times");
+//     hamburger.classList.add("fa-bars");
+//   } else {
+//     btnPrimary.style.backgroundColor = "yellow";
+//     links.classList.add("show-links");
+//     hamburger.classList.remove("fa-bars");
+//     hamburger.classList.add("fa-times");
+//   }
+// });
+
+console.log(navToggle);
